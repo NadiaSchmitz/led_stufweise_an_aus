d4f914d57a64a723230c7aa417c66dcd223a53f5
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-05-18 10:38:03 +0200

    First commit

---
+++ led_stufweise_an_aus.ino
@@ -0,0 +1,19 @@
+int pwmled = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pwmled,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 255; i = i + 3) {
+    analogWrite(pwmled, i);
+    delay(10);  
+  }
+  for (int i = 255; i > 0; i = i - 3) {
+    analogWrite(pwmled, i);
+    delay(10);  
+  }
+  delay(1000);
+}
